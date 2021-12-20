f1cf74e1be8e6d1f7b674c93a913f5791f16feff
cocoding <96038752+pacifica0816@users.noreply.github.com>
2021-12-20 18:37:45 +0900

    commit

--- app.py
+++ app.py
@@ -9,18 +9,24 @@ import random
 
 
 def main() :
-    st.title('최고의 와인을 알아보자!')
+    menu = ['', 'CSV', 'About']
+    st.title('와인리뷰를 알아보자!')
     st.text('쉬라 존맛이라네용 :3')
 
     st.info('신도림 엉베흐 존맛턍인거 다들아시죠? 존맛입니다')
 
+
+
     df = pd.read_csv('data/winemag.csv', index_col=0)
     df.dropna(axis=0, inplace=True)
     df.to_csv('data/winemag.csv')
+    pd.options.display.float_format = '{:.2f}'.format
     st.dataframe(df)
     
-    # '{:.2f}'.format
-    # choice_list = st.multiselect("여러개를 선택할 수 있습니다.", language)
+
+
+
+
 
 
 if __name__ == '__main__' :
